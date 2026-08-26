57e6f388d07ea32d87645bc076e4abb7f473ea05
Divya Sham Sonawane <divyasonawane695@gmail.com>
2026-08-26 18:06:14 +0000

    Add datatypes.js with initial variable declarations and type conversions

---
+++ JS.Hindi/datatypes.js
@@ -0,0 +1,24 @@
+let age= "21"
+let newage= Number(age)
+
+console.log(newage) 
+
+
+Number(true) //1
+Number(false)//0
+Number("23div")//NaN
+Number(undefined)//NaN
+Number(null)//0
+
+
+let name = "as21"
+ 
+let newname= Boolean(name)
+
+console.log(newname);
+console.log(typeof newname);
+
+
+
+
+
